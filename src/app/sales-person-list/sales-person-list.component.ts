da1a8cc985be9f10ae08defbdbb8d413310cb40c
Mateusz Wesołowski <mateusz.wesolowski@eyzee.com>
2019-12-05 13:24:42 +0100

    Added new class and component

---
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {SalesPerson} from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('M', 'W', 'm@w', 2),
+    new SalesPerson('E', 'B', 'e@b', 1),
+    new SalesPerson('X', 'Y', 'x@y', 1)
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
