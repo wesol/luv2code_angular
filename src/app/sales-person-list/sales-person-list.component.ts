b6fec7840ca4c79d3f8083892e2d844f11f13910
Mateusz Wesołowski <mateusz.wesolowski@eyzee.com>
2019-12-05 14:46:57 +0100

    Apdeted values of sales

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -9,9 +9,9 @@ import {SalesPerson} from './sales-person';
 export class SalesPersonListComponent implements OnInit {
 
   salesPersonList: SalesPerson[] = [
-    new SalesPerson('M', 'W', 'm@w', 2),
-    new SalesPerson('E', 'B', 'e@b', 1),
-    new SalesPerson('X', 'Y', 'x@y', 1)
+    new SalesPerson('M', 'W', 'm@w', 20000),
+    new SalesPerson('E', 'B', 'e@b', 30000),
+    new SalesPerson('X', 'Y', 'x@y', 35000)
   ];
 
   constructor() {
